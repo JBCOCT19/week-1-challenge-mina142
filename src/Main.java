dc179650862281402cf705dbcb3be675776a581b
mina142 <mina_salehi53@yahoo.com>
2019-10-24 13:02:23 -0400

    Week One Challenge

--- src/Main.java
+++ src/Main.java
@@ -30,22 +30,25 @@ public class Main {
         } else if (answer.equalsIgnoreCase("i")) {
             System.out.println("Please enter a number: ");
             num = keyboard.nextInt();
-            while (i <= num / 2) {
-                // condition for nonprime number
-                if (num % i == 0) {
-                    flag = true;
-                    break;
+            if (num == 0 || num == 1) {
+                System.out.println(num + " is not prime number");
+            } else {
+                while (i <= num / 2) {
+                    // condition for nonprime number
+                    if (num % i == 0) {
+                        flag = true;
+                        break;
+                    }
+                    ++i;
                 }
-                ++i;
-            }
-            if (flag) {
-                System.out.println(num + " is not a prime number.");
+                if (flag) {
+                    System.out.println(num + " is not a prime number.");
 
+                } else {
+                    System.out.println(num + " is a prime number.");
+                }
             }
-            else {
-                System.out.println(num + " is a prime number.");
-            }
-            }
+        }
         System.out.println("Would you like to try again? (Y/N)  ");
         answer2 = keyboard.next();
         }
